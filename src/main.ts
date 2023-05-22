2c4a09f84c128b80e5c21c9d3454be6a4f242ad0
杨飞 <yangfei86@vip.qq.com>
2023-05-22 18:25:35 +0800

    更新跨域信息

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 开启跨域
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5001', 'http://localhost:5002'],
   });
   await app.listen(3000);
 }
