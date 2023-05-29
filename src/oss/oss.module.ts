742a7e3ee5426cbd0beb6ffc88ae2bec867c9e1b
杨飞 <yangfei86@vip.qq.com>
2023-05-29 18:32:25 +0800

    修复jwt访问异常的问题。

--- src/oss/oss.module.ts
+++ src/oss/oss.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { OSSResolver } from './oss.resolver';
 import { OSSService } from './oss.service';
 import { UserModule } from '@/modules/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule],
   providers: [OSSResolver, OSSService],
   exports: [OSSService],
 })
--- src/oss/oss.resolver.ts
+++ src/oss/oss.resolver.ts
@@ -7,10 +7,10 @@ import { OSSService } from './oss.service';
 import { GqlAuthGuard } from '@/guard/gql.guard';
 
 @Resolver()
-@UseGuards(GqlAuthGuard)
 export class OSSResolver {
   constructor(private readonly ossService: OSSService) {}
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => OSSType, { description: '获取 oss 相关信息' })
   async ossInfo(): Promise<OSSType> {
     return await this.ossService.getSignature();
